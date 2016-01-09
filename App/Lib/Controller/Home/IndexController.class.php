6a9f5eea37cbc818031864c4cb031bc60191e9e1
Gnomail <branew@qq.com>
2016-01-09 16:23:39 +0800

    a test for commit

--- App/Lib/Controller/Home/IndexController.class.php
+++ App/Lib/Controller/Home/IndexController.class.php
@@ -3,7 +3,12 @@ require(LIB_PATH.'Controller'.EXT);
 class IndexController extends Controller{
   public function Index()
   {
-
+    $data   = array(
+      'name' => 'Mike',
+      'age'  => '27'
+    );
+    $result = M('user')->add($data);
+    var_dump($result);
   }
 
   public function Welcome()
